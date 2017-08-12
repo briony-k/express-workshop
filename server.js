11d5697755e4ff17afd4e90e31ad376ee7404def
Briony Kieseker <briony.kieseker@me.com>
2017-08-12 14:36:21 +0000

    BK stuck on Step 8

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var formidable = require('express-formidable');
 
 
 
+app.get('/my-lovely-endpoint', function(req,res){res.send('Hello there!')})
+
 
 app.use(express.static("public")); 
 
@@ -19,3 +21,18 @@ app.post('/create-post',function(req,res){console.log(req.fields)});
 app.listen(8080, function () {
   console.log('Server is listening on port 8080. Ready to accept requests!');
 });
+
+
+
+
+fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+
+    console.log(file.toString());});
+    
+var parsedFile = JSON.parse(file);
+
+app.use(parsedFile());
+
+
+
+
